049fd650ce72a77db2d4979944e96f81b39e99c4
maxim <maxim@MAXIMILIAN>
2024-05-04 15:25:10 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Shop_bestellsystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
